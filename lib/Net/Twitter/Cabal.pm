41803f9536a86b15bdf1f3ed30f06d01f56c9d96
Pedro Figueiredo <me@pedrofigueiredo.org>
2009-02-14 09:47:09 +0000

    version bump for release

--- lib/Net/Twitter/Cabal.pm
+++ lib/Net/Twitter/Cabal.pm
@@ -42,11 +42,11 @@ account's credentials.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
--- lib/Net/Twitter/Cabal/Config.pm
+++ lib/Net/Twitter/Cabal/Config.pm
@@ -27,11 +27,11 @@ Net::Twitter::Cabal::Config - Process Net::Twitter::Cabal's configuration
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
